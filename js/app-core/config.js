bf463dcea15568530e532898825f992a111e2601
Wesley Aspinall <wra427@gmail.com>
2015-11-28 18:47:46 -0500

    added config and parse file

---
+++ js/app-core/config.js
@@ -0,0 +1,24 @@
+let config = function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      templateUrl: 'templates/app-layout/layout.tpl.html'
+    })
+    .state('root.home', {
+      url: '/',
+      controller: 'HomeController as vm',
+      templateUrl: 'templates/app-layout/home.tpl.html'
+    })
+    .state('root.addImage', {
+      url: '/images/add',
+      controller: 'AddImagesController as vm',
+      templateUrl: 'templates/app-images/images-add.tpl.html'
+    });
+};
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default config;
---
+++ js/app-core/constants/parse.constant.js
@@ -0,0 +1,9 @@
+export default {
+    URL: 'https://api.parse.com/1/classes/ngGram',
+    CONFIG: {
+      headers: {
+        'X-Parse-Application-Id': '49GUebXgybdjN3ruwD3TfvWsBuVyWbO7nFSPVQKZ',
+        'X-Parse-REST-API-Key': 'Imt2iOCEDrwsLnauxxEWqrLVCnkiSh2o5BE4IDPJ'
+      }
+    }
+  };
